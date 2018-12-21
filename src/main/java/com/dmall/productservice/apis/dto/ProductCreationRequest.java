be60058e183c0bd6122a155903a30657f8e1eac8
huangliang6 <huangliang6@lenovo.com>
2018-12-21 23:20:07 +0800

    first commit

---
+++ src/main/java/com/dmall/productservice/apis/dto/ProductCreationRequest.java
@@ -0,0 +1,17 @@
+package com.dmall.productservice.apis.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductCreationRequest {
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
---
+++ src/main/java/com/dmall/productservice/apis/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package com.dmall.productservice.apis.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ProductResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
